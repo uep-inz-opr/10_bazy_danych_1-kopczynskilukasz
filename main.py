99297a377c75f028f5cb0e3a9cc4e7d1328aa7d5
LukaszK <kopczynski.l.k@gmail.com>
2022-06-03 11:50:07 +0200

    próba 3

--- main.py
+++ main.py
@@ -1,35 +1,26 @@
+## test 3
+
 import csv
 import sqlite3
 
-class Report_Generator:
-    def __init__(self, connection, string_e= "(%s)"):
-        self.connection= connection
-        self.string_e= string_e
-        self.text_report= None
+con = sqlite3.connect(':memory:', detect_types=sqlite3.PARSE_DECLTYPES|sqlite3.PARSE_COLNAMES)
+cur = con.cursor()
+cur.execute('''CREATE TABLE polaczenia (from_subscriber data_type INTEGER,
+                  to_subscriber data_type INTEGER,
+                  datetime data_type timestamp,
+                  duration data_type INTEGER ,
+                  celltower data_type INTEGER);''')
 
-    def generate_report(self):
-        cursor= self.connection.cursor()
-        sql= f"Select sum(duration) from polaczenia"
-        cursor.execute(sql)
-        result= cursor.fetchone()[0]
-        self.text_report= result
-
-    def get_report(self):
-        return self.text_report
+def read_data(filename):
+    with open(filename, 'r') as fin:
+        reader = csv.reader(fin, delimiter=",")
+        next(reader, None)
+        rows = [x for x in reader]
+        cur.executemany("INSERT INTO polaczenia (from_subscriber, to_subscriber, datetime, duration , celltower) "
+                        "VALUES ( ?, ?, ?, ?, ?);", rows)
+        con.commit()
 
 if __name__ == "__main__":
-    main= input()
-    sqlite_con= sqlite3.connect(':memory:', detect_types=sqlite3.PARSE_DECLTYPES|sqlite3.PARSE_COLNAMES)
-    cur= sqlite_con.cursor()
-    cur.execute('''CREATE TABLE polaczenia (from_subscriber data_type INTEGER, to_subscriber data_type INTEGER, datetime data_type timestamp, duration data_type INTEGER, celltower data_type INTEGER);''') 
-
-    with open(main, 'r') as fin:
-        reader= csv.reader(fin, delimiter= ";")
-        next(reader, None)
-        rows= [x for x in reader]
-        cur.executemany("INSERT INTO polaczenia (from_subscriber, to_subscriber, datetime, duration, celltower) VALUES (?, ?, ?, ?, ?);", rows)
-        sqlite_con.commit()
-
-        rg= Report_Generator(sqlite_con, string_e= "?")
-        rg.generate_report()
-        print(rg.get_report())
+    read_data(input())
+    cur.execute('SELECT sum(duration) FROM polaczenia')
+    print(cur.fetchone()[0])
