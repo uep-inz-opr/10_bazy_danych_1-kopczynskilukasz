e8e5717365a13191d8236bc7a60bfb91860924b6
LukaszK <kopczynski.l.k@gmail.com>
2022-06-03 11:33:18 +0200

    próba 2

--- main.py
+++ main.py
@@ -1,40 +1,35 @@
-## zadanie 1 bazy danych próba 
-
 import csv
 import sqlite3
 
 class Report_Generator:
-    def __init__(self, connection, escape_string= "(%s)"):
+    def __init__(self, connection, string_e= "(%s)"):
         self.connection= connection
-        self.escape_string= escape_string
-        self.report_text= None
+        self.string_e= string_e
+        self.text_report= None
 
     def generate_report(self):
         cursor= self.connection.cursor()
-        sql= f"Select sum from polaczenia"
+        sql= f"Select sum(duration) from polaczenia"
         cursor.execute(sql)
         result= cursor.fetchone()[0]
-        self.report_text= result
+        self.text_report= result
 
     def get_report(self):
-        return self.report_text
+        return self.text_report
 
+if __name__ == "__main__":
+    main= input()
+    sqlite_con= sqlite3.connect(':memory:', detect_types=sqlite3.PARSE_DECLTYPES|sqlite3.PARSE_COLNAMES)
+    cur= sqlite_con.cursor()
+    cur.execute('''CREATE TABLE polaczenia (from_subscriber data_type INTEGER, to_subscriber data_type INTEGER, datetime data_type timestamp, duration data_type INTEGER, celltower data_type INTEGER);''') 
 
-sqlite_con = sqlite3.connect(":memory:", detect_types=sqlite3.PARSE_DECLTYPES|sqlite3.PARSE_COLNAMES)
-cur = sqlite_con.cursor()
-cur.execute('''CREATE TABLE polaczenia (from_subscriber data_type INTEGER,
-                  to_subscriber data_type INTEGER, 
-                  duration data_type INTEGER,
-                  datetime data_type timestamp, 
-                  celltower data_type INTEGER);''')
+    with open(main, 'r') as fin:
+        reader= csv.reader(fin, delimiter= ";")
+        next(reader, None)
+        rows= [x for x in reader]
+        cur.executemany("INSERT INTO polaczenia (from_subscriber, to_subscriber, datetime, duration, celltower) VALUES (?, ?, ?, ?, ?);", rows)
+        sqlite_con.commit()
 
-with open('polaczenia_duze.csv','r') as fin: 
-    reader = csv.reader(fin, delimiter = ";")
-    next(reader, None)
-    rows = [x for x in reader]
-    cur.executemany("INSERT INTO polaczenia (from_subscriber, to_subscriber, datetime, duration , celltower) VALUES (?, ?, ?, ?, ?);", rows)
-    sqlite_con.commit()
-    
-if __name__ == '__main__':
-    rg = Report_Generator(sqlite_con)
-    print(rg.sum_duration())
+        rg= Report_Generator(sqlite_con, string_e= "?")
+        rg.generate_report()
+        print(rg.get_report())
