008bb211e5b68b90f67c09351878350ea020583c
kopczynskilukasz <102819157+kopczynskilukasz@users.noreply.github.com>
2022-06-03 13:44:53 +0200

    Update main.py
    ostatnia poprawka

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ from datetime import date
 if __name__ == "__main__":
     sqlite_con = sqlite3.connect(":memory:", detect_types=sqlite3.PARSE_DECLTYPES|sqlite3.PARSE_COLNAMES)
     cur = sqlite_con.cursor()
-    cur.execute('''CREATE TABLE polaczenia (from_subscriber data_type INTEGER, 
+    cur.execute("""CREATE TABLE polaczenia (from_subscriber data_type INTEGER, 
                     to_subscriber data_type INTEGER, 
                     datetime data_type timestamp, 
                     duration data_type INTEGER , 
-                    celltower data_type INTEGER);''') 
+                    celltower data_type INTEGER);""") 
     filename = input()
     
     with open(filename,'r') as fin: 
@@ -20,7 +20,7 @@ if __name__ == "__main__":
         cur.executemany("INSERT INTO polaczenia (from_subscriber, to_subscriber, datetime, duration , celltower) VALUES (?, ?, ?, ?, ?);",  rows)
         sqlite_con.commit()
         
-    cur.execute("Select sum(duration) from polaczenia ")
+    cur.execute("Select sum(duration) from polaczenia")
     result = cur.fetchone()[0]
 
     print(int(result))
